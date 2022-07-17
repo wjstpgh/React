f737c8894a57774adc8c151ea8299b5ba04f77b1
wjstpgh <sehoJ3756@gmail.com>
2022-07-18 01:03:17 +0900

    api

--- usestate/src/FetchApi.js
+++ usestate/src/FetchApi.js
@@ -3,27 +3,37 @@ import React,{Component} from "react";
 class FetchApiPractice extends Component{
     constructor(){
         super();
-        this.state={forecasts:[],loading:true};
+        this.state={users:[],loading:true};
     }
 
     componentDidMount(){
         this.setState({loading:true});
-        fetch('/')
+        fetch('https://jsonplaceholder.typicode.com/users')
             .then(response=>response.json())
-            .then(data=>{this.setState({forecasts:data,loading:false});});
+            .then(data=>{this.setState({users:data,loading:false});});
     }
 
-    static displayData(forecasts){
-        console.log(forecasts);
+    static displayData(users){
+        console.log(users);
         return(
-            <h1>displayData</h1>
+            <table className="table table-bordered">
+                <tbody> 
+                    {users.map(user=>
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.username}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         );
     }
 
     render(){
-        let contents=this.state.loading ? <p>loading...</p> : FetchApiPractice.displayData(this.state.forecasts);
+        let contents=this.state.loading ? <p>loading...</p> : FetchApiPractice.displayData(this.state.users);
         return(
             <>
+            <h1>FetchAPI</h1>
             {contents}
             </>
         );
--- usestate/src/UseEffectFetchAPI.js
+++ usestate/src/UseEffectFetchAPI.js
@@ -2,19 +2,21 @@ import React,{useState,useEffect} from "react";
 
 function FetchApi(){
 
-    const [forecast,setForecast]=useState(null);
+    const [user,setUser]=useState(null);
     const alarm=', <= Home경로에 전달받은 데이터를 json으로 읽어오는 곳'
 
     useEffect(()=>{
-        fetch('/').then(response=>response.json())
-            .then(setForecast)
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=>response.json())
+            .then(setUser)
             .catch(console.error);
     }, []);
 
     return(
         <>
         <h1>FetchApi</h1>
-        {JSON.stringify(forecast)}
+        {JSON.stringify(user)}
+        <hr></hr>
         {alarm}
         </>
     )
