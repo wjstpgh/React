899b3c1fcb5805e57c1c563b7980ced5873c8ce4
wjstpgh <sehoJ3756@gmail.com>
2022-07-22 16:38:57 +0900

    책 클래스 생성

---
+++ BookApp.React/BookApp.React/BookApp.SqlServer/Books/1Books.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Books]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity(1,1),
+	Title NVarChar(255) Not Null,
+	Description NVarChar(MAX) Null,
+)
+Go
--- BookApp.React/BookApp.React/BookApp.SqlServer/Books/Books.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Books]
-(
-	[Id] INT NOT NULL PRIMARY KEY Identity(1,1),
-	Title NVarChar(255) Not Null,
-	Description NVarChar(MAX) Null,
-)
-Go
