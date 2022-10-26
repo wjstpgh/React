620fe8d83c9ddd5b4cc341b9a97a224f519b150e
wjstpgh <sehoJ3756@gmail.com>
2022-10-26 23:32:39 +0900

    클래스 컴포넌트 렌더링 순서

--- react_hook/src/classComponent.jsx
+++ react_hook/src/classComponent.jsx
@@ -3,9 +3,15 @@ import React, { Component } from "react";
 class ClassComponent extends Component {
   constructor() {
     super();
+    console.log('클래스 생성자');
+  }
+
+  componentDidMount=()=>{
+    console.log('com mount')
   }
 
   render() {
+    console.log('cls render')
     return (
       <div>
         <h1>클래스 컴포넌트</h1>
